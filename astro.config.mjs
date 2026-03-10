ddbd59471f8b6a75280fd84922d9e90bb5847b31
LeonieP25 <leonie.pruniaux@edu.univ-fcomte.fr>
2026-03-10 19:14:30 +0100

    Ajout de l'adaptation a Netlify dans le astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,8 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
+  adapter: netlify(),
 
   image: {
     remotePatterns: [{ 
